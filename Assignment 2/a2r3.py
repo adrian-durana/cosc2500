0ac46686b32f56a3b216c19372d6d4a343fb6657
adrian-durana <jajadurana@hotmail.com>
2024-09-06 11:49:40 +1000

    Second commit - part a completed

--- Assignment 2/a2r3.py
+++ Assignment 2/a2r3.py
@@ -1,40 +1,34 @@
 import numpy as np
 import scipy as sp
 
-## Hilbert matrix
+## Hilbert matrix (A)
 def hilbert(n):
-    matrix = np.fromfunction(lambda i, j: (1/(i+j+1)), (n, n), dtype=int)
-    # Note that you have to change the formula from (1/i+j-1), due to numpy indexing!
-    return matrix
+    return np.fromfunction(lambda i, j: (1/(i+j+1)), (n, n), dtype=int)
+    # Formula has been modified due to NumPy matrix indexing
 
-## Identity column vector 
+## Ones column vector (x)
 def ones(n):
-    vector = np.ones((n,1))
-    return vector
+    return np.ones((n,1))
 
-## b-vector
-def bvector(n):
-    answer = np.matmul(hilbert(n), ones(n))
-    return answer
+## Forward error
+def forward_error(n):
+    b_vector = np.matmul(hilbert(n), ones(n)) # b-vector
+    x_vector = sp.linalg.solve(hilbert(n), b_vector) # x_c vector
+    return np.linalg.norm(np.subtract(ones(n), x_vector), np.inf) # forward error
 
-## Solve for x_c
-def solve(n):
-    solution = sp.linalg.solve(hilbert(n), bvector(n))
-    return solution
+## Error magnification factor
+def error_mag(n):
+    return (forward_error(n) - np.linalg.norm(ones(n), np.inf)) / (np.finfo(float).eps)
+
+## Compare with condition number of A
+def cond(n):
+    return np.linalg.cond(hilbert(n), np.inf)
 
 ## Solve for 2, 5, 10
-print(solve(10))
+print(hilbert(2))
+
 
 # Gaussian elimination (see Sauer, or Blackboard)
-
-# Forward error - infnorm of difference between actual solution and computed solution ||x-x_a||_inf
-
-np.linalg.norm([matrix], ord=inf)
-
-# Error magnification factor - [ infnorm(x-x_c) / infnorm(x) ] / machine-eps.
-
-# Compare with ccondition number of A
-
 
 # Part c
 def matrix_two(n):
@@ -43,9 +37,6 @@ def matrix_two(n):
     return matrix
     # n = 100, 200, 300, 400, 500
 
+
 # Part d:
 # For what values of n do the solutions in the above problems have no correct significant digits?
-
-
-
-
