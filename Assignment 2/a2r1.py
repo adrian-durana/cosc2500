ab50fcf11e185ffee8b5081e1722eee37a4eb796
adrian-durana <jajadurana@hotmail.com>
2024-09-18 16:49:17 +1000

    Modified question 1

--- Assignment 2/a2r1.py
+++ Assignment 2/a2r1.py
@@ -6,49 +6,80 @@
             # (b) the difficulty of choosing starting points or intervals that give convergence
 
 import numpy as np
-import scipy.integrate as sp
 import matplotlib.pyplot as plt
 
-## Function
+## Function and root
+root = 1*np.pi/2
 def function(x):
     return np.cos(x)
-def derivative(x):
-    return -np.sin(x)
 
-## Bisection method (Sauer)
+## Bisection method (Sauer, translated)
 def bisection(a,b): # input: starting interval (a,b)
-    if np.sign(function(a)*function(b)) > 0:
-        raise Exception("The condition f(a)f(b) < 0 is not satisfied.")
-    while (b-a)/2 >= 10**(-6):
+    if np.sign(function(a)) == np.sign(function(b)):
+        raise ValueError("The condition f(a)f(b) < 0 is not satisfied.")
+    iterations = 0
+    while (b-a)/2 >= 10**(-9):
+        iterations += 1
         c = (a+b)/2
         if function(c) == 0: break
         elif function(a)*function(c) < 0: b = c
         else: a = c
-    print('The final interval ['+str(a)+','+str(b)+'] contains a root.')
-    print('The approximate root is' + str((a+b)/2) + '.')
+    return iterations
+    ## print('The approximate root is ' + str((a+b)/2) + '.')
+    ## print('The number of iterations required is ' + str(iterations) + '.')
+    
+## Plot: bisection (fixed midpoint, changing radius)
+def bisection_plot1(points):
+    x_values = np.logspace(-9, np.log10(np.pi), num=points) # For cos(x)
+    y_values = []
+    interval_left = np.array([root]*points) - x_values
+    interval_right = np.array([root]*points) + x_values
+    for i in range(0, points):
+        n = bisection(interval_left[i], interval_right[i])
+        y_values.append(n)
+    plt.semilogx(x_values, y_values)
+    # plt.title("Interval radius versus number of iterations required, fixed midpoint, " + str(points) + "points")
+    plt.ylabel("Number of iterations")
+    plt.xlabel("Interval radius")
+    plt.show() 
 
-## Fixed-point iteration (Sauer)
-def iteration(x, k): # input: x (starting guess), k (iteration steps)
-    array = [x]
-    for i in range(1,k):
-        array.append(function(array[i-1]) + array[i-1])
-    ans = array[k-1]
-    print(ans)
+## Plot: bisection (changing midpoint, fixed radius)
+def bisection_plot2(points, radius):
+    x_values = np.linspace((root-(0.999*radius)),(root+(0.999*radius)), num=points)
+    y_values = []
+    interval_left = x_values - np.array([radius]*points)
+    interval_right = x_values + np.array([radius]*points)
+    for i in range(0, points):
+        n = bisection(interval_left[i], interval_right[i])
+        y_values.append(n)
+    plt.plot(x_values, y_values)
+    # plt.title("Midpoint location versus number of iterations required, fixed radius, " + str(points) + "points")
+    plt.ylabel("Number of iterations")
+    plt.xlabel("Midpoint location")
+    plt.show() 
 
-## Newton's method
-def newton(x, k):
-    array = [x]
-    for i in range(1,k):
-        array.append(array[i-1] - (function(array[i-1]))/(derivative(array[i-1])))
-    ans = array[k-1]
-    print(ans)
+## Fixed-point iteration (Sauer, translated and modified)
+def iteration(x): # input: x (starting guess)
+    iterations = 0
+    while np.abs(x-root) >= 10**(-9):
+        iterations += 1
+        if iterations >= 100: break
+        x = function(x) + x
+    print('The approximate root is ' + str(x) + '.')
+    print('The number of iterations required is ' + str(iterations) + '.')
 
-## Secant method
-def secant(x, k):
-    array = [x]
+## Newton's method, numerical derivative
+def newton(x): # input: x (starting guess)
     h = 6.06*10**(-6)
-    for i in range(1,k):
-        numder = (function(array[i-1]+h) - function(array[i-1]-h)) / (2*h)
-        array.append(array[i-1] - (function(array[i-1]))/numder)
-    ans = array[k-1]
-    print(ans)
+    iterations = 0
+    while np.abs(x-root) >= 10**(-9):
+        iterations += 1
+        if iterations >= 100: break
+        x = x - (function(x) / ((function(x+h) - function(x-h)) / (2*h)))
+    print('The approximate root is ' + str(x) + '.')
+    print('The number of iterations required is ' + str(iterations) + '.')
+
+# bisection(1.561, 1.581)
+# iteration(3*np.pi/2- 10**(-12))
+# newton(2.73635)
+bisection_plot1(10000)
