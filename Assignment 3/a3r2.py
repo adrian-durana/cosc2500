25952f8bb014dbc07ff8730293d5d657013ce17c
Adrian Durana <jajadurana@hotmail.com>
2024-10-01 23:51:06 +1000

    1/10/2024

--- Assignment 3/a3r2.py
+++ Assignment 3/a3r2.py
@@ -53,95 +53,65 @@ x_values = np.linspace(0,1,num=100)
 y_values = solution(x_values)
 plt.plot(x_values, y_values, label = 'exact')
 
-## Finish the plot
-plt.legend()
-plt.show()
-
 
 ##############################
 ## Finite difference method ##
 ##############################
 
-## Numerical second derivative
-def second_derivative():
-    z =
-    return z
-# numder_two = (w[i+1] - 2*w[i] + w[i-1]) / (h**2) - w[i] + (2/3) * np.exp(t) = 0
-
 """
 Consider the following partition of [a,b] with spacing h:
     t[a] = t[0] < t[1] < ... < t[n+1] = t[b]
-
 Let w_i = w(t_i) be an approximation for the correct values y_i = y(t_i) at discrete points t_i.
-
-Substitute values into the numerical approximation for the second derivative:
     
+Substitute values into the numerical approximation for the second derivative, and h = 1/n+1:
+    ( ( w[i+1] - 2*w[i] + w[i-1] ) / h**2 ) = w[i] + (2/3)*np.exp(t[i])
+    w[i-1] - (2 + h**2)*w[i] + w[i+1] = (2/3)*(h**2)*np.exp(t[i])
+    w[i-1] - (2 + (1/(n+1))**2)*w[i] + w[i+1] = (2/3)*((1/(n+1))**2)*np.exp(t[i])
 
+Assume n = 3:
+    w[0] + w[1] * (2 + (1/(n+1))**2) + w[2] = (2/3)*((1/(n+1))**2)*np.exp(t[1])
+    w[1] + w[2] * (2 + (1/(n+1))**2) + w[3] = (2/3)*((1/(n+1))**2)*np.exp(t[2])
+    w[2] + w[3] * (2 + (1/(n+1))**2) + w[4] = (2/3)*((1/(n+1))**2)*np.exp(t[3])
+
+Insert boundary conditions: y(0) = w[0] =  0, y(1) = w[4] = e/3
+    0 + w[1] * (-33/16) + w[2] = (1/24)*np.exp(t[1])
+    w[1] + w[2] * (-33/16) + w[3] = (1/24)*np.exp(t[2])
+    w[2] + w[3] * (-33/16) + (np.exp(1)/3) = (1/24)*np.exp(t[3])
+
+Put into matrix equation
+| -33/16  1     0   |  |w[1]|       | (1/24)*np.exp(t[1]) - 0              |
+|    1  -33/16  1   |  |w[2]|   =   | (1/24)*np.exp(t[2])                  |
+|   0     1  -33/16 |  |w[3]|       | (1/24)*np.exp(t[3]) -  (np.exp(1)/3) |
 """
 
-
-
-# = (w[i+1] - 2*w[i] + w[i-1])  - (h**2)w[i] + (h**2)*(2/3) * np.exp(t_i) = 0
-
-# = w[i-1] + w[i] (-2 - (h**2)) + w[i+1] + (h**2)*(2/3) * np.exp(t_i) = 0
-
-# For n = 3, interval size is 1/4 (1/n+1), three equations
-
-# Insert boundary conditions:
-
-# y(0) = w_0 =  0, y(1) = w_4 =  e/3
-
-# w[0] + w[1] (-2 - (h**2)) + w[2] + (h**2)*(2/3) * np.exp(t_1) = 0
-# w[1] + w[2] (-2 - (h**2)) + w[3] + (h**2)*(2/3) * np.exp(t_2) = 0
-# w[2] + w[3] (-2 - (h**2)) + w[4] + (h**2)*(2/3) * np.exp(t_3) = 0
-    
-# 0 + w[1] (-2 - (h**2)) + w[2] + (h**2)*(2/3) * np.exp(t_1) = 0
-# w[1] + w[2] (-2 - (h**2)) + w[3] + (h**2)*(2/3) * np.exp(t_2) = 0
-# w[2] + w[3] (-2 - (h**2)) + e/3 + (h**2)*(2/3) * np.exp(t_3) = 0
-
-# Then substitute for h (1/4):
-
-# 0 + w[1] (-33/16) + w[2] + (1/24) * np.exp(t_1) = 0
-# w[1] + w[2] (-33/16) + w[3] + (1/24) * np.exp(t_2) = 0
-# w[2] + w[3] (-33/16) + e/3 + (1/24) * np.exp(t_3) = 0
-
-# Put into matrix equation
-
-#| -33/16  1     0   |  |w[1]|       | - (1/24) * np.exp(t_1)        |
-#|    1  -33/16  1   |  |w[2]|   =   | - (1/24) * np.exp(t_2)        |
-#|   0     1  -33/16 |  |w[3]|       | - (1/24) * np.exp(t_3) - e/3  |
-
-
-# Solve by gaussian elimination to find solution values at three points
-
-# For smaller errors, use larger n
-
-## TRANSLATION OF MATLAB CODE HINT!
-
-# How many point do we want?
-#n = 10
-
-# Pre-allocate memory for matrix and vector
-#A = np.zeros((n,n))
-#c = np.zeros((n,1))
-
-#tn = np.linspace(0,1,num=n) # Must be changed for 2a, since it goes to 1.5 ???
-
-#h = t[2] - t[1] ### TRANSLATE FROM MATLAB TO PYTHON
-
-# Boundary conditions
-#A[0,0] = 1
-#c[0] - 0 # First bc
-#A[n-1,n-1] = 1
-#c[n-1] = np.exp(1)/3 # Second bc
+# Preparation
+n = 1000
+h = 1 / (n+1)
+A = np.zeros((n,n)) # 0th column is w[0]
+c = np.zeros((n,1))
+t_n = np.linspace(0,1,num=n)
 
 # Create the matrix
+for i in range(1,n):
+    A[i-1, i-1] = -(2 + (1/(n+1))**2)
+    A[i-1, i] = 1
+    A[i, i-1] = 1
+    c[i-1] = (2/3)*((1/(n+1))**2)*np.exp(t[i-1])
+A[n-1,n-1] = -(2 + (1/(n+1))**2)
 
-#for n in range(2, n):
-#    A[n-1, n-2] = 1 / h**2
-#    A[n-1, n-1] = 1/ h - 2/ h**2
-#    A[n-1, n] = 2/ h ^2 - 1/ h
-#    c[n-1] = tn(n) * np.sin(tn(n))
+# Value of boundary conditions y(0), y(1)
+c[0] = c[0] - 0 # subtract left boundary condition
+c[n-1] = c[n-1] - np.exp(1)/3 # sub. r.b.c
 
 # Solve and plot!
+solution = np.linalg.solve(A, c)
+print(solution)
+plt.plot(t_n, solution, label = 'finite')
 
+## Finish the plot
+plt.legend()
+plt.show()
+
+"""
+Task: generalise for all second-order linear ODEs
+"""
