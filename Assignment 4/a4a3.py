00a481eef0e2395e753943de1ed050afa707be98
adrian-durana <jajadurana@hotmail.com>
2024-10-03 22:43:56 +1000

    3/10/2024

--- Assignment 4/a4a3.py
+++ Assignment 4/a4a3.py
@@ -1,31 +1,24 @@
 import numpy as np
 
 ## Euler-Maruyama method
+def euler_maruyama(h):
+    n = int(1/h)
+    W = np.zeros(n+1) # y(0) = 0
+    rand = np.random.normal(0,1,n)
+    dB = B = np.zeros(n)
+    for i in range(n):
+        dB[i] = rand[i] * np.sqrt(h)
+        B[i] = np.sum(dB)
+        W[i+1] = W[i] + B[i]*h + (9*W[i]**2)**(1/3)
+    return W[-2]
 
-### Calculate the number of steps ###
-a = 0
-b = 1
-h = 1e-1 ## 0.1, 0.01, 0.001
-n = int((b-a) / h)
+## Perform 5000 simulations
+def group(h, n=5000):
+    A = np.zeros(n)
+    for i in range(n): A[i] = euler_maruyama(h)
+    print(np.mean(A))
 
-### Perform 5000 simulations ###
+## Call the function here!
+group(1e-1)
 
-
-### Euler-Maruyama step ###
-w = np.zeros(n+1)
-rand = np.random.normal(0,1,n)
-dB = np.zeros(n)
-B_t = np.zeros(n)
-w[0] = 0 # y(0) = 0
-
-for i in range(n):
-    dB[i] = rand[i] * np.sqrt(h)
-    B_t[i] = np.sum(dB)
-    w[i+1] = w[i] + B_t[i]*h + (9*w[i]**2)**(1/3)
-
-print(w[-1])
-
-
-### Find the mean ###
-
-
+## How to find the error
