a189fdea80fd97a8c4bb3b8a139ba9d5f7c1ec69
adrian-durana <jajadurana@hotmail.com>
2024-10-03 22:43:45 +1000

    3/10/2024

--- Assignment 4/a4a3.py
+++ Assignment 4/a4a3.py
@@ -1,12 +1,31 @@
-Calculate the number of steps .
-Do N times
-    * Euler - Maruyama step *
-Stop
-Find the mean .
-* Euler - Maruyama step *
+import numpy as np
 
-Do number of steps times
-    raw Z from N (0 ,1).
-    Calculate dB and Bt .
-    Apply Euler - Maruyama scheme
-Stop
+## Euler-Maruyama method
+
+### Calculate the number of steps ###
+a = 0
+b = 1
+h = 1e-1 ## 0.1, 0.01, 0.001
+n = int((b-a) / h)
+
+### Perform 5000 simulations ###
+
+
+### Euler-Maruyama step ###
+w = np.zeros(n+1)
+rand = np.random.normal(0,1,n)
+dB = np.zeros(n)
+B_t = np.zeros(n)
+w[0] = 0 # y(0) = 0
+
+for i in range(n):
+    dB[i] = rand[i] * np.sqrt(h)
+    B_t[i] = np.sum(dB)
+    w[i+1] = w[i] + B_t[i]*h + (9*w[i]**2)**(1/3)
+
+print(w[-1])
+
+
+### Find the mean ###
+
+
