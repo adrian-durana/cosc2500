6bd2d8436a8621dd55c31ec75a7693d76cb542ed
adrian-durana <jajadurana@hotmail.com>
2024-10-10 16:31:06 +1000

    10/10/2024

--- Assignment 3/a3r1.py
+++ Assignment 3/a3r1.py
@@ -3,19 +3,17 @@ import scipy as sp
 import matplotlib.pyplot as plt
 
 ## Functions
-F = 1
+F = 3
 def function(t,y):
     if F == 1: return t
     if F == 2: return 2*(t+1)*y
     if F == 3: return 1 / (y*y)
 
 ## Intervals, step size
-interval_start = 0
-interval_end = 1
-y0 = 1
+i1, i2, y0 = 0, 1, 1
 
 ## Exact solutions
-def exact(k, plot = 0, i1 = interval_start, i2 = interval_end):
+def exact(k, plot = 0, i1 = i1, i2 = i2):
     h = 0.1 * (2)**(-k)
     x_values = np.linspace(i1, i2, num = int((i2 - i1)/h))
     if F == 1: exact_values = 0.5 * x_values**2 + 1
@@ -25,29 +23,43 @@ def exact(k, plot = 0, i1 = interval_start, i2 = interval_end):
     return exact_values[-1]
 
 ## Euler's method
-def euler(k, plot = 0, y0 = y0, i1 = interval_start, i2 = interval_end):
+def euler(k, plot = 0, y0 = y0, i1 = i1, i2 = i2):
     h = 0.1 * (2)**(-k)
     t, y, n = [i1], [y0], int((i2-i1)/h)
-    for i in range(0, n-1):
+    for i in range(0, n):
         t.append(t[i]+h)
         y.append(y[i]+h*function(t[i],y[i]))
-    if plot == 1: plt.plot(t,y, label = "Euler")
+    label = str("Euler's method, step size " + str(h))
+    if plot == 1:
+        if k == 1: plt.plot(t,y, label = label, color = '#FF6347')
+        elif k == 2: plt.plot(t,y, label = label, color = '#FFA500')
+        elif k == 3: plt.plot(t,y, label = label, color = '#FFD700')
+        elif k == 4: plt.plot(t,y, label = label, color = '#ADFF2F')
+        elif k == 5: plt.plot(t,y, label = label, color = 'g')
+        else: plt.plot(t,y, label = label, color = 'red')
     return y[-1]
 
 ## Fixed step Runge-Kutta method
-def rk_f(k, plot = 0, y0 = y0, i1 = interval_start, i2 = interval_end):
+def rk_f(k, plot = 0, y0 = y0, i1 = i1, i2 = i2):
     h = 0.1 * (2)**(-k)
     n = int((i2-i1)/h)
-    t = np.transpose(np.linspace(i1, i2, n))
+    t = np.transpose(np.linspace(i1, i2, n  +1))
     y = [y0]
-    for n in range(1, n):
+    for n in range(1, n+1):
         k1 = h*function(t[n-1], y[n-1])
         k2 = h*function(t[n-1] + 0.5*h, y[n-1] + 0.5*k1)
         k3 = h*function(t[n-1] + 0.5*h, y[n-1] + 0.5*k2)
         k4 = h*function(t[n-1] + h, y[n-1] + k3)
         y.append(y[n-1] + (1/6)*(k1 + 2*k2 + 2*k3 + k4))
     y = np.transpose(y)
-    if plot == 1: plt.plot(t,y, label = "Fixed R-K")
+    label = str("Fixed-step RK method, step size " + str(h))
+    if plot == 1:
+        if k == 1: plt.plot(t,y, label = label, color = '#FF6347')
+        elif k == 2: plt.plot(t,y, label = label, color = '#FFA500')
+        elif k == 3: plt.plot(t,y, label = label, color = '#FFD700')
+        elif k == 4: plt.plot(t,y, label = label, color = '#ADFF2F')
+        elif k == 5: plt.plot(t,y, label = label, color = 'g')
+        else: plt.plot(t,y, label = label, color = 'red')
     return y[-1]
 
 ## Log-log plot of error
@@ -59,28 +71,38 @@ def error():
         eval[k] = exact(k)
         A[k] = euler(k)
         B[k] = rk_f(k)
-    eu = np.subtract(eval, A)
-    rk = np.subtract(eval, B)
+    eu = np.abs(np.subtract(eval, A))
+    rk = np.abs(np.subtract(eval, B))
+    plt.xlabel("Step size")
+    plt.ylabel("Error")
     plt.loglog(H, eu, label = "Euler")
-    plt.loglog(H, rk, label = "Runge-Kutta")
+    plt.loglog(H, rk, label = "Fixed-step RK")
 
 ## Adaptive step Runge-Kutta method
-def rk_a(y0 = y0, i1 = interval_start, i2 = interval_end):
-    sol = sp.integrate.solve_ivp(function, [interval_start, interval_end], [y0], rtol = 1e-9, atol = 1e-9, max_step = 1e-3)
+def rk_a(y0 = y0, i1 = i1, i2 = i2):
+    sol = sp.integrate.solve_ivp(function, [i1, i2], [y0], max_step = 1e-1)
     sol_x = sol.t
     sol_y = sol.y[0]
-    plt.plot(sol_x, sol_y, label = "Adaptive R-K")
+    plt.plot(sol_x, sol_y, label = "Adaptive RK")
 
 ## Plots
-exact(0, 1)
-euler(0, 1)
-rk_f(0, 1)
+"""
+euler(0,1)
+euler(1,1)
+euler(2,1)
+euler(3,1)
+euler(4,1)
+euler(5,1)
+rk_f(0,1)
+rk_f(1,1)
+rk_f(2,1)
+rk_f(3,1)
+rk_f(4,1)
+rk_f(5,1)
 rk_a()
-
+exact(10,1)
 error()
+"""
 
 plt.legend() 
 plt.show()
-
-
-
--- Assignment 3/a3r2.py
+++ Assignment 3/a3r2.py
@@ -4,68 +4,58 @@ import matplotlib.pyplot as plt
 
 ## Variables
 a, b = 0, 1
-
 F = 3
 if F == 1: y_a, y_b = 0, np.exp(1)/3
 elif F == 2: y_a, y_b = 1, np.exp(1)
 elif F == 3: y_a, y_b = np.exp(1)**3, 1
-else: raise ValueError("Check F")
 
-#####################
-## Shooting method ##
-#####################
-
-## System of first-order ODEs
+## Shooting method
 def system(t,y): # y[0]' = y[1], y[1]' = ...
     if F == 1: return [y[1], y[0] + 2*np.exp(t)/3] 
     elif F == 2: return [y[1], y[0]*(2 + (4*t**2))]
     elif F == 3: return [y[1], 3*y[0] - 2*y[1]]
 
-## Guessing second initial condition
-def sGuess(s):
-    solution = sp.solve_ivp(system, [a,b], [y_a, s], t_eval=[b])
+def guess(s): # Shooting method guesses
+    solution = sp.solve_ivp(system, [a,b], [y_a, s], t_eval=[b], max_step = 1e-1)
     return float(solution.y[0,-1] - y_b)
 
-## Solving ODE using bisection
-def sSolve(g1, g2):
-    def bisection(A, B, tol = 1e-9, maxiterations = 1000):
-        if np.sign(sGuess(A)) == np.sign(sGuess(B)):
-            raise ValueError("The condition f(a)f(b) < 0 is not satisfied.")
-        for i in range(0, int(maxiterations)):
-            c = (A+B)/2
-            if sGuess(A)*sGuess(c) < 0: B = c
-            else: A = c
-            if np.abs(A-B) < tol: break
-        return c
-    sol = sp.solve_ivp(system, [a, b], [y_a, bisection(g1,g2)], rtol = 1e-9, atol = 1e-9, max_step = 1e-3)
+def bisection(A, B, tol = 1e-9, maxiterations = 1000): # Root-finding
+    if np.sign(guess(A)) == np.sign(guess(B)):
+        raise ValueError("The condition f(a)f(b) < 0 is not satisfied.")
+    for i in range(0, int(maxiterations)):
+        c = (A+B)/2
+        if guess(A)*guess(c) < 0: B = c
+        else: A = c
+        if np.abs(A-B) < tol: break
+    return c
+
+def shooting(g1, g2): # Solving the IVP
+    sol = sp.solve_ivp(system, [a, b], [y_a, bisection(g1,g2)], max_step = 1e-6)
     plt.plot(sol.t, sol.y[0], label = 'Shooting method')
+    #plt.title('Shooting method')
 
-##############################
-## Finite difference method ##
-##############################
-
+## Finite difference method
 def finite(n):
     A = np.zeros((n,n))
     c = np.zeros(n)
     t = np.linspace(a,b,num=n)
 
     if F == 1: factor = (1/(n+1))**2
-    elif F == 2: factor = (2+4*t[i-1]**2) / (n+1)**2
     elif F == 3: factor = 3 / (n+1)**2
-
+    
     for i in range(1,n):
         A[i-1, i] = A[i, i-1] = 1
         if F == 1:
             A[i-1, i-1] = -2 - factor
             c[i-1] = (2/3)*factor*np.exp(t[i-1])
         elif F == 2:
+            factor = (2+4*t[i-1]**2) / (n+1)**2
             A[i-1, i-1] = -2 - factor
         elif F == 3:
             A[i-1, i-1] = -2 - factor
             A[i-1, i] = (1 + 1 / (n+1))
             A[i, i-1] = (1 - 1 / (n+1))
     A[n-1,n-1] = -2 - factor
-    
     if F == 3: 
         c[0] -= (1 - 1 / (n+1))*y_a
         c[n-1] -= (1 + 1 / (n+1))*y_b
@@ -74,13 +64,23 @@ def finite(n):
         c[n-1] -= y_b
 
     solution = np.linalg.solve(A, c)
-    plt.plot(t, solution, label = 'Finite difference')
+    plt.plot(t, solution, color='orange', label = 'Finite difference method')
+    #plt.title('Finite difference method')
+
+## Exact solution of ODE
+def exact():
+    def solution(x):
+        if F == 1: return (1/3)*x*np.exp(x) 
+        elif F == 3: return np.exp(3-3*x)
+    x_values = np.linspace(0,1, num = int(1e6))
+    y_values = solution(x_values)
+    plt.plot(x_values, y_values, label = 'Exact solution', color='green')
+
+# Call functions here
+shooting(-100, 100)
+finite(1000)
+exact()
 
 ## Plotting
 plt.legend()
 plt.show()
-
-# Call functions here
-sSolve(-100, 100)
-finite(1000)
-
